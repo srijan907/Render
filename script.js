2c00ff6e40fc8abf1314aad435e4a2ff50b6b3bb
Woker-Xd <sreejanxd1@gmail.com>
2025-03-16 21:44:04 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("scanBtn").addEventListener("click", function() {
+    alert("Scanning and generating Session ID...");
+});
+
+document.getElementById("deployBtn").addEventListener("click", function() {
+    alert("Redirecting to deployment options...");
+});
